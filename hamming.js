753cae3801474b61e07fdbe0fd378650b3a3cafc
Stefan Mehnert <stefanmehnert@gmail.com>
2015-11-15 06:25:33 +0100

    Refactored to a functional Solution

--- hamming.js
+++ hamming.js
@@ -1,16 +1,29 @@
 var computeHammingDistance = function(s, t) {
-	
-	var hammingDistance = 0;
-	
-	if(s.length != t.length)
+		
+	if(areStrandLengthsNotEqual(s,t))
 		throw new Error("DNA strands must be of equal length.");
 	
-	for(var i = 0; i <= s.length; i++) {
-		if(s[i] != t[i])
-			hammingDistance++;
+	return forStrands(s, t, incrementIfMutation);
+}
+
+function areStrandLengthsNotEqual(s, t) {
+	return (s.length != t.length) ? true : false;
+}
+
+function forStrands(s, t, increment) {
+	var hammingDistance = 0;
+	
+	for(var i = 0; i <= s.length; i++ ) {
+		hammingDistance += increment(s, t, i);
 	}
 	
 	return hammingDistance;
+}
+	
+function incrementIfMutation(s, t, i) {
+	if(s[i] != t[i])
+		return 1;
+	return 0;
 }
 
 module.exports = computeHammingDistance;
