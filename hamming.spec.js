d6def924d23beab0b4e56ea2c93fc466a354289c
Stefan Mehnert <stefanmehnert@gmail.com>
2015-11-15 05:46:32 +0100

    All comparion Tests enabled (green)

--- hamming.spec.js
+++ hamming.spec.js
@@ -10,23 +10,23 @@ describe('Hamming', function () {
     expect(compute('A','G')).toEqual(1);
   });
 
-  xit('complete hamming distance for small strand', function () {
+  it('complete hamming distance for small strand', function () {
     expect(compute('AG','CT')).toEqual(2);
   });
 
-  xit('small hamming distance', function () {
+  it('small hamming distance', function () {
     expect(compute('AT','CT')).toEqual(1);
   });
 
-  xit('small hamming distance in longer strand', function () {
+  it('small hamming distance in longer strand', function () {
     expect(compute('GGACG', 'GGTCG')).toEqual(1);
   });
 
-  xit('large hamming distance', function () {
+  it('large hamming distance', function () {
     expect(compute('GATACA', 'GCATAA')).toEqual(4);
   });
 
-  xit('hamming distance in very long strand', function () {
+  it('hamming distance in very long strand', function () {
     expect(compute('GGACGGATTCTG', 'AGGACGGATTCT')).toEqual(9);
   });
 
